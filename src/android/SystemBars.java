90054ec6224b7e778929f2a205fe79d6314b1e7b
최준수_KBS <choijs@kbs.co.kr>
2026-09-08 15:30:13 +0900

    화면 전환 시 상태바 글자색상 변경 이슈 수정

--- src/android/SystemBars.java
+++ src/android/SystemBars.java
@@ -3,7 +3,7 @@ package cordova.plugins.systembars;
 import android.app.Activity;
 import android.view.View;
 import android.view.Window;
-
+import android.content.res.Configuration;
 import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.core.view.WindowInsetsControllerCompat;
