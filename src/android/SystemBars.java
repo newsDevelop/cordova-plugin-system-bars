e3da66783cc68a81a900f7757ca241b92701d637
최준수_KBS <choijs@kbs.co.kr>
2026-09-08 10:30:11 +0900

    Add Systembar status darkmode

--- src/android/SystemBars.java
+++ src/android/SystemBars.java
@@ -1,4 +1,4 @@
-package cordova.plugins.screenorientation;
+package cordova.plugins.systembars;
 
 import android.app.Activity;
 import android.view.View;
@@ -27,6 +27,12 @@ public class SystemBars extends CordovaPlugin {
 
         if ("show".equals(action)) {
             showSystemBars(callbackContext);
+            return true;
+        }
+
+        if ("setDarkMode".equals(action)) {
+            boolean darkMode = args.optBoolean(0, false);
+            setDarkMode(darkMode, callbackContext);
             return true;
         }
 
@@ -100,6 +106,45 @@ public class SystemBars extends CordovaPlugin {
 
             } catch (Exception e) {
                 callbackContext.error("Failed to show system bars: " + e.getMessage());
+            }
+        });
+    }
+
+    /**
+     * 다크모드에 따라 StatusBar / NavigationBar 아이콘 색상 변경
+     *
+     * darkMode = true
+     *   -> 밝은 아이콘
+     *
+     * darkMode = false
+     *   -> 어두운 아이콘
+     */
+    private void setDarkMode(boolean darkMode, CallbackContext callbackContext) {
+
+        Activity activity = cordova.getActivity();
+
+        activity.runOnUiThread(() -> {
+
+            try {
+
+                Window window = activity.getWindow();
+
+                WindowInsetsControllerCompat controller = WindowCompat.getInsetsController(window, window.getDecorView());
+
+                /*
+                 * true  -> 검은색 아이콘
+                 * false -> 흰색 아이콘
+                 */
+                controller.setAppearanceLightStatusBars(!darkMode);
+
+                callbackContext.success();
+
+            } catch (Exception e) {
+
+                callbackContext.error(
+                        "Failed to change system bar appearance: "
+                                + e.getMessage()
+                );
             }
         });
     }
--- www/SystemBars.js
+++ www/SystemBars.js
@@ -28,3 +28,17 @@ exports.show = function (success, error) {
         []
     );
 };
+
+
+/**
+ * 다크모드 시 상태바 텍스트 색상 변경
+ */
+exports.setDarkMode = function (darkMode, success, error) {
+    exec(
+        success,
+        error,
+        'SystemBars',
+        'setDarkMode',
+        [darkMode]
+    );
+};
